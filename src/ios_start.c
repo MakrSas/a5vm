a71562c74dc3daa63c3afcc5ed85636f21432d60
Makr <ngrom33@gmail.com>
2026-08-10 21:28:55 +0300

    fix freestanding ARMv7 entrypoint

--- src/ios_start.c
+++ src/ios_start.c
@@ -1,6 +1,6 @@
 extern int a5vm_ios_main(void);
 
-__attribute__((noreturn)) void _start(void) {
+__attribute__((noreturn)) void start(void) {
     register int result __asm__("r0") = a5vm_ios_main();
     register int syscall_number __asm__("r12") = 1;
     __asm__ volatile("svc #0x80"
